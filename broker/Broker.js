e8b86d836da492c58572ae6be11c090e9da1ba61
Edoardo Oranger <edo.oranger@gmail.com>
2021-02-10 18:46:55 +0100

    UPDATE! File Broker.js, created an interface

--- broker/Broker.js
+++ broker/Broker.js
@@ -1,7 +1,20 @@
 const aedes = require('aedes')();
 const server = require('net').createServer(aedes.handle);
-const port = 8080;
 
-server.listen(port, function () {
-    console.log('Server started and listening on port ' + port);
-});
+
+class Broker {
+    constructor() {
+        this.port = 8080;
+        this.startBroker();
+    }
+
+    startBroker() {
+        if (server.listening == false) {
+            server.listen(this.port, function () {
+                console.log('Server started and listening on port ' + this.port);
+            });
+        }
+    }
+}
+
+exports.Broker = Broker
